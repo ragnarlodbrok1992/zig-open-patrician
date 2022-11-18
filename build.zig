4a9fcf8eedc33c5b2ff9b75d33c60a9d82fd2df5
ragnarlodbrok1992 <maraus92@gmail.com>
2022-11-18 23:44:47 +0100

    First commit!

---
+++ build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const glfw = @import("libs/mach-glfw/build.zig");
+
+pub fn build(b: *std.build.Builder) !void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("open-patrician", "src/main.zig");
+
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.addPackage(glfw.pkg);
+    exe.install();
+
+    try glfw.link(b, exe, .{});
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the game");
+    run_step.dependOn(&run_cmd.step);
+
+    // Not tests for now
+    // Some text input from new keyboard
+}
---
+++ src/main.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+const warn = std.debug.warn;
+const panic = std.debug.panic;
+const glfw = @import("glfw");
+
+const WIDTH: i32 = 800;
+const HEIGHT: i32 = 600;
+// var RUN_ENGINE: bool = true;
+
+// Math constructs for engine graphics
+const Vec2 = struct {
+    x: f64,
+    y: f64,
+
+    pub fn init(x: f64, y: f64) Vec2 {
+        return Vec2{
+            .x = x,
+            .y = y,
+        };
+    }
+};
+
+const Vec3 = struct {
+    x: f64,
+    y: f64,
+    z: f64,
+
+    pub fn init(x: f64, y: f64, z: f64) Vec3 {
+        return Vec2{
+            .x = x,
+            .y = y,
+            .z = z,
+        };
+    }
+};
+
+// Shaders
+const vert_shader: []const u8 =
+    \\#version 330
+    \\uniform mat4 MVP;
+    \\in vec3 vCol;
+    \\in vec3 vPos;
+    \\out vec3 color;
+    \\void main()
+    \\{
+    \\  gl_Position = MVP * vec(vPos, 0.0, 1.0);
+    \\  color = vCol;
+    \\}
+;
+const frag_shader: []const u8 =
+    \\#version 330
+    \\in vec3 color;
+    \\out vec4 fragment;
+    \\void main()
+    \\{
+    \\  fragment = vec4(color, 1.0);
+    \\}
+;
+
+// Debug switch
+const DEBUG = true;
+
+// Key callback function
+fn keyCallback(window: glfw.Window, key: glfw.Key, scancode: i32, action: glfw.Action, mods: glfw.Mods) void {
+    // _ = window;
+    // _ = key;
+    _ = scancode;
+    _ = action;
+    _ = mods;
+
+    if (key == glfw.Key.escape) {
+        //RUN_ENGINE = false;
+        window.setShouldClose(true);
+    }
+}
+
+fn mouseClickCallback(window: glfw.Window, button: glfw.MouseButton, action: glfw.Action, mods: glfw.Mods) void {
+    _ = window;
+    _ = button;
+    _ = action;
+    _ = mods;
+}
+
+fn mouseCursorPositionCallback(window: glfw.Window, xpos: f64, ypos: f64) void {
+    _ = window;
+    _ = xpos;
+    _ = ypos;
+}
+
+pub fn main() !u8 {
+    // Print out version - for development right now
+    const version: i32 = 0;
+    const stdout = std.io.getStdOut().writer();
+
+    try stdout.print("Open Patrician - pre-alpha version {d}.", .{version});
+
+    // Glfw init
+    try glfw.init(.{});
+    defer glfw.terminate();
+
+    // Mach opengl
+    const window = try glfw.Window.create(WIDTH, HEIGHT, "Open Patrician - pre-alpha version.", null, null, .{});
+    defer window.destroy();
+
+    // Set key callback
+    window.setKeyCallback(keyCallback);
+    window.setMouseButtonCallback(mouseClickCallback);
+    window.setCursorPosCallback(mouseCursorPositionCallback);
+
+    // Waiting for closing the window
+    while (!window.shouldClose()) {
+        try glfw.pollEvents();
+    }
+
+    return 0;
+}
